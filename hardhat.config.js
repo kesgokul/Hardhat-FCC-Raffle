a83237e909c3e1d0883eff0db6ef4f13568fb22f
kesgokul <gokulkesavan5005@gmail.com>
2022-10-26 20:48:41 +0530

    staging edit

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,21 @@ require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PKEY = process.env.GOERLI_PKEY;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      chainId: 5,
+      accounts: [PKEY],
     },
   },
   namedAccounts: {
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,6 +7,7 @@ const networkConfig = {
     gasLane:
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
     entranceFee: ethers.utils.parseEther("0.01"),
+    VRFCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
     subscriptionId: "0",
     callbackGasLimit: "500000",
     interval: "30",
